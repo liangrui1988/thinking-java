345eef27aa262eb31229276aa96389785ddcc93c
liangrui <liangrui_1988@126.com>
2015-07-19 20:36:23 +0800

    scheduler

--- src/org/rui/thread/newc/GreenhouseScheduler.java
+++ src/org/rui/thread/newc/GreenhouseScheduler.java
@@ -1,5 +1,7 @@
 package org.rui.thread.newc;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -39,6 +41,10 @@ public class GreenhouseScheduler
 	 */
 	public void scheduler(Runnable event, long delay)
 	{
+		
+		/**
+		 * 建并执行在给定延迟后启用的一次性操作。
+		 */
 		scheduler.schedule(event, delay, TimeUnit.MILLISECONDS);
 	}
 
@@ -46,10 +52,15 @@ public class GreenhouseScheduler
 	 * 重复
 	 * @param envent
 	 * @param initialDelay
-	 * @param period 周期
+	 * @param period 连续执行之间的周期    时间越少 执行的越快
 	 */
 	public void repeat(Runnable envent, long initialDelay, long period)
 	{
+		
+		/**
+		 *  创建并执行一个在给定初始延迟后首次启用的定期操作，后续操作具有给定的周期；也就是将在 initialDelay
+		 *   后开始执行，然后在 initialDelay+period 后执行，接着在 initialDelay + 2 * period 后执行，依此类推。
+		 */
 		scheduler.scheduleAtFixedRate(envent, initialDelay, period,
 				TimeUnit.MILLISECONDS);
 	}
@@ -66,8 +77,9 @@ public class GreenhouseScheduler
 		@Override
 		public void run()
 		{
-			// 供货上的灯
-			System.out.println("Rurning on lights");
+			//System.out.println("Turning on lights");
+			System.out.println("打开电灯");
+
 			light = true;
 
 		}
@@ -83,11 +95,13 @@ public class GreenhouseScheduler
 	{
 
 		// put hardware control code here to 把硬件控制代码在这里
-		// physically turn off the light. 身体 关灯。
+		// physically turn off the light. 身关灯。
 		@Override
 		public void run()
 		{
-			System.out.println("turning greenhouse water on");
+			System.out.println("旋转 关灯 ");
+			//			System.out.println("Turning off  light");
+
 			water = true;
 		}
 	}
@@ -98,7 +112,9 @@ public class GreenhouseScheduler
 		@Override
 		public void run()
 		{
-			System.out.println("Turning greenhouse water on");
+			//System.out.println("Turning greenhouse water on");
+			System.out.println("温室水开");
+
 			water = true;
 
 		}
@@ -111,7 +127,9 @@ public class GreenhouseScheduler
 		@Override
 		public void run()
 		{
-			System.out.println("Turning greenhouse water off");
+			System.out.println("温室水关");
+
+			//System.out.println("Turning greenhouse water off");
 			water = false;
 
 		}
@@ -123,14 +141,15 @@ public class GreenhouseScheduler
 	 * @author lenovo
 	 *
 	 */
-	class ThernostatNight implements Runnable
+	class ThermostatNight implements Runnable
 	{
 
 		@Override
 		public void run()
 		{
-			// put hardware control code here
-			System.out.println("thermostat to night setting");
+			// put hardware control code here 把硬件控制代码在这里
+			//System.out.println("thermostat to night setting");
+			System.out.println("自动控温器  夜晚设置");
 			setThermostat("Night");
 
 		}
@@ -148,7 +167,9 @@ public class GreenhouseScheduler
 		public void run()
 		{
 			// put hardware control code here
-			System.out.println("thermostat to day setting");
+			System.out.println("温室白天 设置");
+			//			System.out.println("thermostat to day setting");
+
 			setThermostat("Day");
 
 		}
@@ -165,7 +186,7 @@ public class GreenhouseScheduler
 		@Override
 		public void run()
 		{
-			System.out.println("Bing!");
+			System.out.println("Bing!响铃>>");
 		}
 
 	}
@@ -181,7 +202,7 @@ public class GreenhouseScheduler
 		@Override
 		public void run()
 		{
-			System.out.println("Terminate");
+			System.out.println("Terminate》》结束");
 			scheduler.shutdown();
 			// must start a separate task to do this job 必须启动一个单独的任务来做这份工作
 			// since the scheduler has been shut down 自调度器已经关闭
@@ -191,7 +212,7 @@ public class GreenhouseScheduler
 				{
 					for (DataPoint d : data)
 					{
-						System.out.println(d);
+						System.out.println("DataPoint:"+d);
 					}
 				};
 			}.start();
@@ -199,7 +220,14 @@ public class GreenhouseScheduler
 		}
 	}
 
-	// inner class
+	
+	
+	/**
+	 * 可以持有并显示单个的数据段
+	 * @author lenovo
+	 *
+	 */
+	// inner class 
 	static class DataPoint
 	{
 		final Calendar time;
@@ -221,7 +249,8 @@ public class GreenhouseScheduler
 
 		public String toString()
 		{
-			return time.getTime()
+			DateFormat fd=new SimpleDateFormat("yyyy/MM/dd hh:mm ss");
+			return fd.format(time.getTime())
 					+ String.format("temperature:%1$.1f humidity:%2$.2f",
 							temperature, humidity);
 		}
@@ -236,14 +265,16 @@ public class GreenhouseScheduler
 		lastTime.set(Calendar.SECOND, 00);
 	}
 
-	private float lastTemp = 65.0f;
-	private int tempDirection = +1;
-	private float lastHumidity = 50.0f;
-	private int humidityDirection = +1;
+	private float lastTemp = 65.0f;//
+	private int tempDirection = +1;//温度 方位
+	private float lastHumidity = 50.0f;//最后的  湿度
+	private int humidityDirection = +1;//湿气 方位
 	private Random rand = new Random(47);
 	List<DataPoint> data = Collections
 			.synchronizedList(new ArrayList<DataPoint>());
 
+	
+	//被调度的任务，它在每次运行时，都可以产生仿真数据，并将其添加到Greenhouse的list<DataPoint>中
 	// ineer class
 	class CollectData implements Runnable
 	{
@@ -251,7 +282,7 @@ public class GreenhouseScheduler
 		@Override
 		public void run()
 		{
-			System.out.println("CollectData");
+			System.out.println("CollectData》》》run");
 			synchronized (GreenhouseScheduler.this)
 			{
 				// pretend the interval is longer than it is: 假装间隔时间比是:
@@ -287,21 +318,116 @@ public class GreenhouseScheduler
 	public static void main(String[] args)
 	{
 		GreenhouseScheduler gh = new GreenhouseScheduler();
+		
+		//延迟多少时间  关闭
 		gh.scheduler(gh.new Terminate(), 5000);
+		
 		// former restart class not necessary:前重启类没有必要:
-		gh.repeat(gh.new Bell(), 0, 1000);
-		gh.repeat(gh.new ThernostatNight(), 0, 2000);
-		gh.repeat(gh.new LightOn(), 0, 200);
+		gh.repeat(gh.new Bell(), 0, 1000);//响铃
+		gh.repeat(gh.new ThermostatNight(), 0, 2000);//夜晚  2秒执行
+		
+		gh.repeat(gh.new LightOn(), 0, 200);//灯
 		gh.repeat(gh.new LightOff(), 0, 400);
 
-		gh.repeat(gh.new WaterOn(), 0, 600);
-
+		gh.repeat(gh.new WaterOn(), 0, 600);//水
 		gh.repeat(gh.new WaterOff(), 0, 800);
-
-		gh.repeat(gh.new ThernostatDay(), 0, 1400);
+//
+	    gh.repeat(gh.new ThernostatDay(), 0, 1400);//白天
 		gh.repeat(gh.new CollectData(), 500, 500);
 
 	}
 }
+/***
+ * output:
+ * Bing!响铃>>
+自动控温器  夜晚设置
+打开电灯
+旋转 关灯 
+温室水开
+温室水关
+温室白天 设置
+打开电灯
+打开电灯
+旋转 关灯 
+CollectData》》》run
+温室水开
+打开电灯
+打开电灯
+旋转 关灯 
+温室水关
+Bing!响铃>>
+打开电灯
+CollectData》》》run
+打开电灯
+温室水开
+旋转 关灯 
+打开电灯
+温室白天 设置
+CollectData》》》run
+打开电灯
+温室水关
+旋转 关灯 
+打开电灯
+温室水开
+Bing!响铃>>
+CollectData》》》run
+旋转 关灯 
+打开电灯
+自动控温器  夜晚设置
+打开电灯
+打开电灯
+旋转 关灯 
+温室水开
+温室水关
+CollectData》》》run
+打开电灯
+打开电灯
+旋转 关灯 
+温室白天 设置
+打开电灯
+CollectData》》》run
+温室水开
+Bing!响铃>>
+旋转 关灯 
+温室水关
+打开电灯
+打开电灯
+CollectData》》》run
+旋转 关灯 
+温室水开
+打开电灯
+打开电灯
+Bing!响铃>>
+自动控温器  夜晚设置
+旋转 关灯 
+温室水关
+CollectData》》》run
+打开电灯
+打开电灯
+温室水开
+温室白天 设置
+打开电灯
+旋转 关灯 
+CollectData》》》run
+打开电灯
+打开电灯
+温室水关
+温室水开
+旋转 关灯 
+Bing!响铃>>
+打开电灯
+CollectData》》》run
+Terminate》》结束
+DataPoint:2015/07/19 09:00 00temperature:66.4 humidity:50.05
+DataPoint:2015/07/19 09:30 00temperature:68.0 humidity:50.47
+DataPoint:2015/07/19 10:00 00temperature:69.7 humidity:51.42
+DataPoint:2015/07/19 10:30 00temperature:70.8 humidity:50.87
+DataPoint:2015/07/19 11:00 00temperature:72.0 humidity:50.32
+DataPoint:2015/07/19 11:30 00temperature:73.2 humidity:49.92
+DataPoint:2015/07/20 12:00 00temperature:71.9 humidity:49.81
+DataPoint:2015/07/20 12:30 00temperature:70.1 humidity:50.25
+DataPoint:2015/07/20 01:00 00temperature:68.9 humidity:51.00
+DataPoint:2015/07/20 01:30 00temperature:67.7 humidity:50.21
 
+ */
 
